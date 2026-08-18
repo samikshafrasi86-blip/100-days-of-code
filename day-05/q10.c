8e0f1122b6a8f225fede5fcbba6d6e46e53f076d
Samiksha <samiksha@Samikshas-MacBook-Air.local>
2026-08-18 15:27:24 +0530

    add 100-days-of-code

---
+++ day-05/q10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int seconds, hours, minutes;
+
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    seconds = seconds % 3600;
+
+    minutes = seconds / 60;
+    seconds = seconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, seconds);
+
+    return 0;
+}
