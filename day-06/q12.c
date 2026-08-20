2657cff81c1697bcc3dd94744b8d513324030294
samikshafrasi86-blip <samikshafrasi86@gmail.com>
2026-08-20 12:00:00 +0530

    Added Day 6

---
+++ day-06/q12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        if (num == 0) {
+            printf("Zero");
+        } else {
+            printf("Positive");
+        }
+    } else {
+        printf("Negative");
+    }
+
+    return 0;
+}
