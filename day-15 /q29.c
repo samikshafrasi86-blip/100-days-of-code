50d71100adfbac37ab79d4dda656b58e2e00514c
Samiksha <samiksha@Samikshas-MacBook-Air.local>
2026-08-24 20:04:10 +0530

    Add day-15

---
+++ day-15 /q29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long factorial = 1;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("%lld", factorial);
+
+    return 0;
+}
