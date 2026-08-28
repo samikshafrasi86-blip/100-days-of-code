33203e3b8f3cba30449d801a1b178e2810d83321
samikshafrasi86-blip <samikshafrasi86@gmail.com>
2026-08-28 12:00:00 +0530

    Added Day 14

---
+++ day-14/q28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product = product * i;
+    }
+
+    printf("%lld", product);
+
+    return 0;
+}
